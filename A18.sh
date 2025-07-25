59a78ddfe6497d5db39d014ceac7ed54a10b17a0
Aaron3299 <111169665+Aaron3299@users.noreply.github.com>
2025-07-25 20:30:01 +0800

    Update A18.sh

--- A18.sh
+++ A18.sh
@@ -3,6 +3,8 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 
 #修改机器名称
 sed -i 's/Openwrt/A18/g' package/base-files/files/bin/config_generate
+#TYYD免登录
+sed -i -r 's#/bin/login#/bin/login -f root#g' feeds/packages/utils/ttyd/files/ttyd.config
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf package/emortal/themes/luci-theme-argon
